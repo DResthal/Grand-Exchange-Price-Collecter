6a1f4424bd8eb0d34e572b4fb26f7ba9f043957a
DralrinResthal <c.neilclack@gmail.com>
2020-12-09 21:42:41 -0600

    Replace missing code. Begin getting items

--- api_parser/explore.py
+++ api_parser/explore.py
@@ -2,21 +2,44 @@ import requests as req
 import math
 from datetime import datetime
 
-categories_url = "https://secure.runescape.com/m=itemdb_rs/api/catalogue/category.json?category="
-items_url = "https://secure.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=0&alpha=a&page=1"
+categories_url = "https://secure.runescape.com/m=itemdb_rs/api/catalogue/category.json?category=0"
+# items url = "https://secure.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=0&alpha=a&page=1"
 
-def get_item_attr(data: dict) -> dict:
-    for i in data:
+# Create dict for each group in category endpoint
+def get_category_groups(raw: dict, cat: int) -> dict:
+    item_attr = {
+        "category": cat,
+        "alpha": raw["letter"],
+        "numItems": raw["items"],
+        "numPages": None
+    }
 
-        item_attr = {
-            "letter": i["letter"],
-            "numItems": i["items"],
-            "numPages": None
-        }
+    pages = math.ceil(item_attr["numItems"] / 12)
 
-    print(f"Category: {i}")
-    get_item_attr(item_dict)
+    item_attr["numPages"] = pages
+
+    return item_attr
+
+
+# Get each item from each 
+def get_items(item_attr: dict) -> dict:
     
-    
+    for p in range(item_attr["numPages"]):
+        url = f"https://secure.runescape.com/m=itemdb_rs/api/catalogue/items.json?category={item_attr['category']}&alpha={item_attr['alpha']}&page={p}"
+        res = req.get(url)
+        print(url)
+
+
+
+# 42 was a manually discovered integer for max number of category pages. 43 returns null
+for c in range(4):
+    res_data = req.get(categories_url + str(c)).json()["alpha"]
+    #print(res_data)
+    for i in res_data:
+        #print(get_category_groups(i, c))
+        get_items(get_category_groups(i, c))
+
+
+
 
 print(f"Done at {datetime.now()}")
