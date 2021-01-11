10db50d797981a1ad868bb96d658b9011236e343
DralrinResthal <c.neilclack@gmail.com>
2021-01-11 14:52:51 -0600

    Fixed csv write
    CSV now only writes headers once per file, main loop is within 'with open' function.
    Some breaking code changes have been repaired as well. Previous commit was pushed after failure to test

--- api_parser/explore.py
+++ api_parser/explore.py
@@ -2,23 +2,25 @@ import requests as req
 import math
 import json
 import csv
-import os
 from datetime import datetime
 
 # import json
 
-categories_url = "https://secure.runescape.com/m=itemdb_rs/api/catalogue/category.json?category=0"
+categories_url = (
+    "https://secure.runescape.com/m=itemdb_rs/api/catalogue/category.json?category=0"
+)
 # items url = "https://secure.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=0&alpha=a&page=1"
+today = datetime.now().strftime("%m-%d-%Y-%H-%M")
 
 # Pass each category's groups of items to another function
 def get_groups(cat_response, cat_num):
 
     # Encode '#' for later use in the URL
-    if cat_response[0]["letter"] == '#':
-        cat_response[0]["letter"] = '%23'
+    if cat_response[0]["letter"] == "#":
+        cat_response[0]["letter"] = "%23"
 
     for i in range(len(cat_response)):
-        letter = cat_response[i]['letter']
+        letter = cat_response[i]["letter"]
         num_items = cat_response[i]["items"]
 
         # Find the number of pages there are based on the number of items (of which there are 12 per page), round up to nearest whole
@@ -31,83 +33,73 @@ def get_groups(cat_response, cat_num):
             pass
 
 
-
 def get_items_list(letter, num_pages, cat_num):
 
     for page in range(num_pages):
-        url = f'https://secure.runescape.com/m=itemdb_rs/api/catalogue/items.json?category={cat_num}&alpha={letter}&page={page}'
+        url = f"https://secure.runescape.com/m=itemdb_rs/api/catalogue/items.json?category={cat_num}&alpha={letter}&page={page}"
         raw_items = req.get(url).json()
-
-        # logging all things before processing
-        # I want to avoid writing this, when necessary, but because it is a loop, it is proving somewhat challenging
-        # checking if the file exists would stop all other categories from writing to the page, but not checking
-        # wastes time by writing to this csv every single run...
-        # I'm honestly not sure if I will keep this level of logging in the future, why do I need it?
-        with open('all_items.csv', 'a') as f:
-            writer = csv.writer(f, delimiter=',')
-            writer.writerow(raw_items)
-
         get_single_items(raw_items)
 
 
 def get_single_items(raw_items):
     converted_to_dict = json.dumps(raw_items)
-    items = json.loads(converted_to_dict)['items']
-    today = datetime.now().strftime("%m-%d-%Y-%H")
-    csv_columns = ['id', 'name', 'description', 'type', 'current_price', 'members']
+    items = json.loads(converted_to_dict)["items"]
+    save_data(items)
 
+
+def save_data(items):
     for item in items:
-
         item_dict = {
-        'id' : item['id'],
-        'name' : item['name'],
-        'description' : item['description'],
-        'type' : item['type'],
-        'current_price' : item['current']['price'],
-        'members' : item['members']
+            "id": item["id"],
+            "name": item["name"],
+            "description": item["description"],
+            "type": item["type"],
+            "current_price": item["current"]["price"],
+            "members": item["members"],
         }
+        file_writer.writerow(item_dict)
 
-        # logging individual items
-        with open(f'{today}_single_items.csv', 'a') as f:
-            file_writer = csv.DictWriter(f, delimiter=',', fieldnames=csv_columns)
-            file_writer.writeheader()
-            file_writer.writerow(item_dict)
 
-# Iterate over each category, I have manually descovered that there are 42 categories, 43+ returns null
-# Example response
-# [
-#    {'letter': '#', 'items': 3},
-#    {'letter': 'a', 'items': 65},
-#    {'letter': 'b', 'items': 82},
-#    {'letter': 'c', 'items': 91},
-#    {'letter': 'd', 'items': 53},
-#    {'letter': 'e', 'items': 15},
-#    {'letter': 'f', 'items': 24},
-#    {'letter': 'g', 'items': 46},
-#    {'letter': 'h', 'items': 22},
-#    {'letter': 'i', 'items': 22},
-#    {'letter': 'j', 'items': 2},
-#    {'letter': 'k', 'items': 10},
-#    {'letter': 'l', 'items': 21},
-#    {'letter': 'm', 'items': 39},
-#    {'letter': 'n', 'items': 6},
-#    {'letter': 'o', 'items': 25},
-#    {'letter': 'p', 'items': 95},
-#    {'letter': 'q', 'items': 0},
-#    {'letter': 'r', 'items': 41},
-#    {'letter': 's', 'items': 118},
-#    {'letter': 't', 'items': 30},
-#    {'letter': 'u', 'items': 1},
-#    {'letter': 'v', 'items': 22},
-#    {'letter': 'w', 'items': 19},
-#    {'letter': 'x', 'items': 1},
-#    {'letter': 'y', 'items': 2},
-#    {'letter': 'z', 'items': 9}
-# ]
+with open(f"{today}_single_items.csv", "a") as f:
+    csv_columns = ["id", "name", "description", "type", "current_price", "members"]
+    file_writer = csv.DictWriter(f, delimiter=",", fieldnames=csv_columns)
+    file_writer.writeheader()
 
-for i in range(43):
-    print(f"cat: {i}")
-    category_response = req.get(categories_url + str(i)).json()["alpha"]
-    get_groups(category_response, i)
+    # Iterate over each category, I have manually discovered that there are 42 categories, 43+ returns null
+    # Example response
+    # [
+    #    {'letter': '#', 'items': 3},
+    #    {'letter': 'a', 'items': 65},
+    #    {'letter': 'b', 'items': 82},
+    #    {'letter': 'c', 'items': 91},
+    #    {'letter': 'd', 'items': 53},
+    #    {'letter': 'e', 'items': 15},
+    #    {'letter': 'f', 'items': 24},
+    #    {'letter': 'g', 'items': 46},
+    #    {'letter': 'h', 'items': 22},
+    #    {'letter': 'i', 'items': 22},
+    #    {'letter': 'j', 'items': 2},
+    #    {'letter': 'k', 'items': 10},
+    #    {'letter': 'l', 'items': 21},
+    #    {'letter': 'm', 'items': 39},
+    #    {'letter': 'n', 'items': 6},
+    #    {'letter': 'o', 'items': 25},
+    #    {'letter': 'p', 'items': 95},
+    #    {'letter': 'q', 'items': 0},
+    #    {'letter': 'r', 'items': 41},
+    #    {'letter': 's', 'items': 118},
+    #    {'letter': 't', 'items': 30},
+    #    {'letter': 'u', 'items': 1},
+    #    {'letter': 'v', 'items': 22},
+    #    {'letter': 'w', 'items': 19},
+    #    {'letter': 'x', 'items': 1},
+    #    {'letter': 'y', 'items': 2},
+    #    {'letter': 'z', 'items': 9}
+    # ]
 
-print(f"Completed at: {datetime.now().strftime("%m/%d/%Y %H:%M:%S")}"
+    for i in range(43):
+        print(f"cat: {i}")
+        category_response = req.get(categories_url + str(i)).json()["alpha"]
+        get_groups(category_response, i)
+
+print(f"Completed at: {datetime.now().strftime('%m/%d/%Y %H:%M:%S')}")
