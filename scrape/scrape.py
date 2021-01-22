a908677d341243ba64b7d7b6e5d52a9e15a55ffc
DralrinResthal <c.neilclack@gmail.com>
2021-01-22 23:35:16 +0000

    Add to dataframe and save to csv

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -29,12 +29,12 @@ def setup_logger(name, log_file, level=logging.INFO):
     return logger
 
 
-error_log = setup_logger('e_log', "error.log", level=logging.WARNING)
-application_log = setup_logger('a_log', "app.log", level=logging.INFO)
+error_log = setup_logger("e_log", "error.log", level=logging.WARNING)
+application_log = setup_logger("a_log", "app.log", level=logging.INFO)
 
 
 def log_error(c_msg, err, url="N/A", res="N/A"):
-    custom_message = f'{c_msg}\n\n{err}'
+    custom_message = f"{c_msg}\n\n{err}"
     error_log.warning(custom_message)
     error_log.warning(f"Affected URL: {url}")
     error_log.warning(f"URL Response: {res}")
@@ -63,45 +63,72 @@ def requests_retry_session(
     session.mount("https://", adapter)
     return session
 
-### Main Loop setup ###
+
+########################################################################################
+
+application_log.info(
+    f'Starting data scrape: {datetime.now().strftime("%m/%d/%Y %H:%M:%S")}'
+)
+print(f'Starting data scrape: {datetime.now().strftime("%m/%d/%Y %H:%M:%S")}')
+
 categories_url = (
     "https://secure.runescape.com/m=itemdb_rs/api/catalogue/category.json?category="
 )
 items_url = "https://secure.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=0&alpha=a&page=1"
 
+
 def process_item_groups(cat_resp, cat_num):
-    for g in category_list:
-        group = g
+    group_list = []
+    for group in cat_resp:
         # Encode for url
-        url_encoded_group = parse_items.url_encode(g)
+        url_encoded_group = parse_items.url_encode(group)
         # Add number of pages
         with_num_pages = parse_items.add_num_of_pages(url_encoded_group)
-        
-        # Fetch items
-        for p in range(with_num_pages['num_of_pages']):
-            print(parse_items.build_url(with_num_pages, p+1, i))
 
-## Main Loop ##
-for i in range(43):
+        # Build urls for each page
+        group_urls = []
+        if with_num_pages["num_of_pages"] != 0:
+            for p in range(with_num_pages["num_of_pages"]):
+                group_urls.append(parse_items.build_url(with_num_pages, p + 1, cat_num))
+        else:
+            pass
+
+        group.update({"urls": group_urls})
+        group_list.append(group)
+
+    group_list_dataframe = pd.DataFrame(group_list)
+    return group_list_dataframe
+
+
+### Main Loop (42) ###
+# Empty df for appending all groups
+columns = ['letter', 'items', 'num_of_pages', 'urls']
+end_df = pd.DataFrame(columns=columns)
+
+for i in range(42):
     print(f"Fetching Category {i}")
     try:
         # Returns a list of dicts
         current_url = categories_url + str(i)
         category_list = req.get(current_url).json()["alpha"]
-        application_log.info(f'Category {i}: \n{category_list}')
-    except requests.exceptions.Timeout as e:
-        log_error('Request timeout', e, current_url)
+        application_log.info(f"Category {i}: \n{category_list}")
+    except req.exceptions.Timeout as e:
+        log_error("Request timeout", e, current_url)
         pass
-    except requests.exceptions.TooManyRedirects as e:
-        log_error('Too Many Redirects, check URL', e, current_url)
+    except req.exceptions.TooManyRedirects as e:
+        log_error("Too Many Redirects, check URL", e, current_url)
         pass
-    except requests.exceptions.RequestException as e:
-        log_error('Unknown Exception', e, current_url)
+    except json.decoder.JSONDecodeError as e:
+        log_error("JSON Decode error", e, current_url, req.get(current_url).content)
+    except req.exceptions.RequestException as e:
+        log_error("Unknown Exception", e, current_url)
         pass
-    
-    process_item_groups(category_list, i)
 
+    df = process_item_groups(category_list, i)
+    df = df.replace(0, np.nan).dropna()
+    end_df = pd.concat([end_df, df], sort=False)
 
+end_df.to_csv('item_groups.csv')
 
-
-print(f"Completed at: {datetime.now().strftime('%m/%d/%Y %H:%M:%S')}")
+application_log.info(f'Completed at: {datetime.now().strftime("%m/%d/%Y %H:%M:%S")}')
+print(f'Completed at: {datetime.now().strftime("%m/%d/%Y %H:%M:%S")}')
